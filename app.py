4bff0d5efbe8adaa1367a9bfb3c3d73972c727f3
ademirMaltazard <ademir.mk98@gmail.com>
2024-12-04 21:05:13 -0300

    adição de novas rotas

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/login")
 def login():
-    return render_template("index.html")
+    return render_template("login/index.html")
 
-app.run()
+@app.route("/")
+def home():
+    return render_template("Home/index.html")
+
+@app.route("/admCadastrarProdutos")
+def productRegister():
+    return render_template("productRegister/index.html")
+
+if __name__ == "__main__":
+    app.run()
