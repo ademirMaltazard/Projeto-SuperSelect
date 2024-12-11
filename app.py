57206865097d837d076244c9d03c2344da7258a2
ademirMaltazard <ademir.mk98@gmail.com>
2024-12-11 18:56:14 -0300

    adição de dados para teste, ajuste nas variaveis, adição de rota para listagem dos produtos

--- app.py
+++ app.py
@@ -2,7 +2,23 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-produtos = []
+# dados para testes
+user = [{
+    'name': 'Ademir',
+    'type': 'admin',
+    'username': 'Maltazard'
+    },{
+    'name': 'Zard',
+    'type': 'usuario',
+    'username': 'Zard'
+    }
+]
+
+products = [{'id':'00', 'name': 'teste', 'description': 'teste', 'category': 'teste', 'price': 'teste', 'expiration': 'teste'},
+            {'id':'00', 'name': 'teste', 'description': 'teste', 'category': 'teste', 'price': 'teste', 'expiration': 'teste'},
+            {'id':'00', 'name': 'teste', 'description': 'teste', 'category': 'teste', 'price': 'teste', 'expiration': 'teste'}]
+
+
 
 @app.route("/login")
 def login():
@@ -10,9 +26,12 @@ def login():
 
 @app.route("/")
 def home():
-    return render_template("Home/index.html")
+    print(user[1])
+    if user[1].get('type') == 'admin':
+        print('administrador')
+    return render_template("Home/index.html", user= user[1])
 
-@app.route("/admCadastrarProdutos", methods = ['GET', 'POST'])
+@app.route("/admCadastrarProdutos", methods= ['GET', 'POST'])
 def productRegister():
     if request.method == 'POST':
         name = request.form.get('name')
@@ -20,10 +39,16 @@ def productRegister():
         category = request.form.get('category')
         price = request.form.get('price')
         expiration = request.form.get('expiration')
-        produto = {'name': name, 'description': description, 'category': category, 'price': price, 'expiration': expiration}
-        produtos.append(produto)
+        #ID deve ser adicionada automatimente pelo banco de dados
+        product = {'id' : '00', 'name': name, 'description': description, 'category': category, 'price': price, 'expiration': expiration}
+        products.append(product)
 
-    return render_template("productRegister/index.html", produtos= produtos, name= request.form.get('name'))
+    return render_template("productRegister/index.html", products= products, name= request.form.get('name'))
+
+@app.route("/listagemProdutos", methods= ['GET', 'POST'])
+def productsList():
+    return render_template("Products/index.html", products= products)
+
 
 if __name__ == "__main__":
     app.run()
