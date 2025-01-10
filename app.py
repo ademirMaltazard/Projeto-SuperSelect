1f1f907009309433b06bd817c760c02e4aa7e9ed
AdemirMaltazard <ademir.mk98@gmail.com>
2025-01-10 20:19:37 -0300

    importação e ultilização das bibliotecas dotenv e hashlib, criação e alteração: palavras chaves, função de encriptar e registro de novo usuario

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask_login import LoginManager, login_user, login_required, logout_user
+from dotenv import load_dotenv
 from database.db import db
 from database.Models import *
+import hashlib
 
 app = Flask(__name__)
 ln = LoginManager(app)
+load_dotenv()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/superselect'
+MYSQL_URI = os.getenv('MYSQL_URI')
+APP_SECRET_KEY = os.getenv('APP_SECRET_KEY')
+KEY_ADMIN = os.getenv('KEY_ADMIN')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = MYSQL_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'chaveTeste'
+app.secret_key = APP_SECRET_KEY
 db.init_app(app)
 ln.login_view = 'login'
 
 # dados para testes
 products = [{'id':'00', 'name': 'teste', 'description': 'teste', 'category': 'teste', 'price': 'teste', 'expiration': 'teste'}]
+
+#criptografar senha
+def hash(text):
+    hash_obj = hashlib.sha256(text.encode('utf8'))
+    return hash_obj.hexdigest()
 
 @ln.user_loader
 def userLoader(id):
@@ -26,7 +39,7 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
 
-        user = db.session.query(User).filter_by(email=email, senha=password).first()
+        user = db.session.query(User).filter_by(email=email, senha=hash(password)).first()
         if not user:
             print('erro')
             alert = "Usuario ou senha invalidos!"
@@ -42,6 +55,35 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+@app.route('/registrar-se', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        userName = request.form.get('userName')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        password = request.form.get('password')
+        code = request.form.get('code')
+        type = 'comum'
+
+        if code == KEY_ADMIN:
+            type = 'admin'
+
+        search_user = db.session.query(User).filter_by(email=email).first()
+        if search_user:
+            alert = 'email já cadastrado, realize o login!'
+            return redirect(url_for('login', alert=alert))
+
+        password_hash = hash(password)
+        newUser = User(nome= name, senha=password_hash, nomeUsuario=userName, tipo=type, email=email, telefone=phone)
+        db.session.add(newUser)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    return render_template("register/index.html")
+
 
 @app.route("/")
 @login_required
@@ -83,7 +125,7 @@ def productsList():
                            length= len(products),
                            totalPages= totalPages,
                            currentPage= currentPage,
-                           user= currentUser)
+                           user = {"type": "admin" })
 
 
 if __name__ == "__main__":
